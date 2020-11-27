938cf49f5e38a72b792539d5d37297900902649a
Th3RapidK1ller <stcikeri@bx.fallmerayer.it>
2020-11-27 17:47:17 +0100

    Getting Infos trival

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,3 +5,9 @@ J
 src/sample/Controller.java,a\9\a952ee24ccaf329067caf45d8e61416ce808b0ea
 D
 src/sample/Main.java,a\d\ad8e8bd4ff8f02a914e3d9ca1bcc32dfc8c503a9
+V
+&out/production/SNMP/sample/sample.fxml,0\9\0945bc74945f8686c3b74df8f54f236afaced2d2
+8
+SNMP.iml,1\3\13f359416c41d92af19d7bd7f10c3af212d1639f
+O
+.idea/libraries/tnm4j_1_0_9.xml,d\a\da933346d10eaea520266b234761f31c9a616748
---
+++ src/main/java/sample/Main.java
@@ -0,0 +1,59 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import org.soulwing.snmp.*;
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Mib mib = MibFactory.getInstance().newMib();
+        mib.load("SNMPv2-SMI");
+        mib.load("SNMPv2-TC");
+        mib.load("SNMPv2-CONF");
+        mib.load("SNMP-FRAMEWORK-MIB");
+        mib.load("SNMP-TARGET-MIB");
+        mib.load("SNMP-NOTIFICATION-MIB");
+        mib.load("IANA-ITU-ALARM-TC-MIB");
+        mib.load("ITU-ALARM-TC-MIB");
+        mib.load("INET-ADDRESS-MIB");
+        mib.load("RFC1155-SMI");
+        mib.load("RFC1158-MIB");
+        mib.load("RFC-1212");
+        mib.load("RFC1213-MIB");
+        mib.load("RMON-MIB");
+        mib.load("RFC1271-MIB");
+        mib.load("TOKEN-RING-RMON-MIB");
+        mib.load("RMON2-MIB");
+        mib.load("ALARM-MIB");
+
+
+        SimpleSnmpV2cTarget target = new SimpleSnmpV2cTarget();
+        target.setAddress("192.168.1.41");
+        target.setCommunity("public");
+
+        SnmpContext context = SnmpFactory.getInstance().newContext(target, mib);
+        try {
+            VarbindCollection result = context.get(".1.3.6.1.2.1.1.1.0").get();
+            System.out.println(result.get(0));
+        } finally {
+            context.close();
+        }
+
+        BorderPane root = new BorderPane();
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
